4c47bd7b85f36df5b3b4a019ead9324b4a0a81f8
Junda Liu <junda@celer.network>
2019-10-31 22:53:24 -0700

    exp about anyresolver

---
+++ bar.go
@@ -0,0 +1,39 @@
+package bar
+
+import (
+	"reflect"
+	"strings"
+
+	"github.com/golang/protobuf/proto"
+)
+
+var BetterAnyResolver *ar
+
+type ar struct{}
+
+func (*ar) Resolve(typeUrl string) (proto.Message, error) {
+	mname := typeUrl
+	if slash := strings.LastIndex(mname, "/"); slash >= 0 {
+		mname = mname[slash+1:]
+	}
+	mt := proto.MessageType(mname)
+	if mt == nil {
+		return (*valMsg)(nil), nil
+	}
+	return reflect.New(mt.Elem()).Interface().(proto.Message), nil
+}
+
+type valMsg struct {
+	v []byte
+}
+
+func (*valMsg) ProtoMessage()             {}
+func (*valMsg) XXX_WellKnownType() string { return "BytesValue" }
+func (m *valMsg) Reset()                  { *m = valMsg{} }
+func (m *valMsg) String() string {
+	return ""
+}
+func (m *valMsg) Unmarshal(b []byte) error {
+	m.v = append(m.v, b...)
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/i9/bar
+
+go 1.13
+
+require github.com/golang/protobuf v1.3.2
